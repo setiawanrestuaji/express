9a8d9d7c3ec552b25bf39da8095ca1b4e2bb15f5
setiawanrestuaji <setiawanrestuaji123@gmail.com>
2020-09-07 09:06:42 +0700

    using http

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express()
+// const app = express()
 
-app.use('/', (req, res) => {
-  res.json({
-    msg: 'success'
-  })
-})
+// app.use('/', (req, res) => {
+//   res.json({
+//     msg: 'success'
+//   })
+// })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+// app.listen(3000, () => {
+//   console.log('Server running on port 3000')
+// })
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+}).listen(3000, "0.0.0.0");
+
+console.log('Server running at http://192.168.1.1:80/');
